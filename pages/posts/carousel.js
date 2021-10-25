67d540746d309877b09906f97260f7760ae9989a
jonnywillrees <jonnywillrees@gmail.com>
2021-10-25 08:23:15 +0200

    refactor index page. add gray-matter dep. configure fetching data from file system.

---
+++ pages/posts/carousel.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Carousel() {
+
+
+
+  return (
+    <div>
+      <Head>
+        <title>Carousel</title>
+      </Head>
+      Carousel placeholder
+    </div>
+  )
+}
+
+export default Carousel;
